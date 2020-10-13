27a5c3b11f66a1da940f202375ea3614af7d11db
Koen <koenvwel@gmail.com>
2020-10-13 19:37:45 +0200

    Fix linting, and some refactoring

--- synpy/__init__.py
+++ synpy/__init__.py
@@ -1,6 +1,7 @@
 """
 This is a simple program that 'visualizes' video material into sound.
-It's a total rip-off from 3b1b's video on Hilbert curves: https://www.youtube.com/watch?v=3s7h2MHQtxc
+It's a total rip-off from 3b1b's video on Hilbert curves:
+https://www.youtube.com/watch?v=3s7h2MHQtxc
 A couple of functions should be accelerated by your GPU but results may vary.
 
 Author: Koen van Wel
@@ -14,15 +15,13 @@ import struct
 import sys
 import time
 import threading
-import numba
 from collections import deque
-from typing import Iterable, List
-
+from typing import Iterable, List, Deque
+import numba
 import numpy as np
-import cv2
 import pyaudio
-
 from hilbertcurve.hilbertcurve import HilbertCurve
+import cv2
 
 CHANNELS = 1
 RATE = int(44100 / 2)
@@ -35,10 +34,12 @@ INVERTED = False
 NUMBA_PARALLEL = False
 MAX_BUFFER = 10  # Amount of frames the audio stream can fall behind
 SHOW_VIDEO = True
-SHOW_PLOT = False
+VIDEO_MAX_WIDTH = 480
+VIDEO_MAX_HEIGHT = 320
+SHOW_PLOT = True
 FOURIER_TRANSFORM_WAVE = True
 
-_MAX_USED_FREQ = min(MAX_FREQ, int(RATE/2))
+_MAX_USED_FREQ = min(MAX_FREQ, int(RATE / 2))
 _HILBERT_RESOLUTION = 2 ** HILBERT_PRECISION
 
 if SHOW_PLOT:
@@ -93,7 +94,7 @@ def frame_to_wave(frame: np.ndarray, hilbert_curve: HilbertCurve, delta: float) 
     """
     assert frame.shape == (_HILBERT_RESOLUTION, _HILBERT_RESOLUTION)
     vec = frame_to_gain_vector(frame, hilbert_curve)
-    return vector_to_wave(list(vec), delta)
+    return vector_to_wave(numba.typed.List(vec), delta)
 
 
 def frame_to_gain_vector(frame: np.ndarray, hilbert_curve: HilbertCurve) -> Iterable[int]:
@@ -109,7 +110,13 @@ def frame_to_gain_vector(frame: np.ndarray, hilbert_curve: HilbertCurve) -> Iter
 
 
 @numba.njit(parallel=NUMBA_PARALLEL)
-def vector_to_wave(vector: numba.types.float64[:], delta: float):
+def vector_to_wave(vector: numba.types.float64[:], delta: float) -> np.ndarray:
+    """
+    Create a waveform based on a vector of gains
+    :param vector: vector of gains between 0-255
+    :param delta: time of the waveform
+    :return: np.ndarray[numba.int64] representing a waveform
+    """
     frame_count = int(RATE * delta)
     summed_wave = np.zeros(frame_count, numba.float64)
     for i in numba.prange(len(vector)):
@@ -124,12 +131,26 @@ def vector_to_wave(vector: numba.types.float64[:], delta: float):
 
 
 @numba.njit  # Parallelization cannot be helpful
-def get_amplitude(gain: int, frequency: float):
+def get_amplitude(gain: int, frequency: float) -> float:
+    """
+    Not yet implemented
+    Get a frequency based on a gain between 0-255 and a frequency. Always under MAX_AMPLITUDE
+    :param gain:
+    :param frequency:
+    :return: an amplitude
+    """
     return (gain / 255) * MAX_AMPLITUDE  # TODO create equalizer
 
 
 @numba.njit(parallel=NUMBA_PARALLEL)
-def generate_freq(frequency: float, frame_count: int, amplitude: float):
+def generate_freq(frequency: float, frame_count: int, amplitude: float) -> np.ndarray:
+    """
+    Numba function to generate a specific frequency and return it as a np.ndarray
+    :param frequency: The frequency that is to be generated
+    :param frame_count: The amount of frames to fill
+    :param amplitude: The amplitude of the frequency
+    :return: np.ndarray[numba.float64] of the values the frequency is made up of
+    """
     wave_data = np.zeros(frame_count, numba.float64)
     for i in numba.prange(frame_count):
         frames_per_wave = RATE / frequency
@@ -138,7 +159,7 @@ def generate_freq(frequency: float, frame_count: int, amplitude: float):
     return wave_data
 
 
-def wave_to_data(wave: List[int]):
+def wave_to_data(wave: List[int]) -> bytes:
     """
     Take a list of values in a wave and pack them in a C struct
     :param wave: the audio wave
@@ -148,15 +169,15 @@ def wave_to_data(wave: List[int]):
     return struct.pack(number_of_bytes + 'h', *wave)
 
 
-def plot_to_ndarray(data):
+def plot_to_ndarray(data) -> np.ndarray:
     """
     Plot a figure to an ndarray
     :param data: data to be plotted
     :return: np.ndarray compatible with cv2 (RGBA)
     """
     fig = plt.figure()
-    ax = plt.subplot(111)
-    ax.plot(data)
+    line = plt.subplot(111)
+    line.plot(data)
     io_buf = io.BytesIO()
     fig.savefig(io_buf, format='raw')
     io_buf.seek(0)
@@ -169,11 +190,68 @@ def plot_to_ndarray(data):
     return img_arr
 
 
-def synesthesia(path):
-    pa = pyaudio.PyAudio()
-    stream = pa.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
-    video = cv2.VideoCapture(path)
+def stream_from_buffer(stream: pyaudio.Stream, buffer: Deque) -> bool:
+    """
+    Reads buffer and writes to pyaudio stream
+    Also shows the video and the plot if SHOW_VIDEO and SHOW_PLOT are True respectively
+    :param stream: pyaudio.Stream to write to
+    :param buffer: buffer to read from (pops a wave, frame, prepared_frame triplet)
+    :return: False if the user closed the window by pressing 'q', True otherwise
+    """
+    if len(buffer) > 0:
+        wave, frame, prepared_frame = buffer.popleft()
+        stream.write(wave_to_data(wave))
 
+        if SHOW_VIDEO:
+            height, width = frame.shape[1::-1]
+            if width > VIDEO_MAX_WIDTH or height > VIDEO_MAX_HEIGHT:
+                frame = cv2.resize(frame, (VIDEO_MAX_WIDTH, VIDEO_MAX_HEIGHT))
+
+            resized_prepared_frame = cv2.cvtColor(
+                cv2.resize(prepared_frame, frame.shape[1::-1]),
+                cv2.COLOR_GRAY2RGB
+            )
+            concat = cv2.vconcat([frame, resized_prepared_frame])
+
+            if SHOW_PLOT:
+                if FOURIER_TRANSFORM_WAVE:
+                    wave = np.fft.rfft(wave).real
+
+                plot = cv2.cvtColor(
+                    cv2.resize(plot_to_ndarray(wave), concat.shape[1::-1]),
+                    cv2.COLOR_RGBA2RGB
+                )
+                cv2.imshow("Video", cv2.hconcat([concat, plot]))
+            else:
+                cv2.imshow("Video", concat)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return False
+
+        elif SHOW_PLOT:
+            if FOURIER_TRANSFORM_WAVE:
+                wave = np.fft.rfft(wave).real
+            plt.clf()
+            plt.plot(wave)
+            plt.pause(0.0001)
+
+    if len(buffer) > MAX_BUFFER:
+        for _ in range(int(MAX_BUFFER / 2)):
+            buffer.popleft()
+
+    return True
+
+
+def synesthesia(path: str):
+    """
+    Read video from a path. Create a thread that changes each frame into a
+    waveform and puts that waveform in a buffer
+    Read that buffer from the main thread and write it to the audio stream
+    :param path: path to video
+    """
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+    video = cv2.VideoCapture(path)
     buffer = deque()
 
     def wave_buffering():
@@ -182,59 +260,33 @@ def synesthesia(path):
 
     wave_buffering_thread = threading.Thread(target=wave_buffering)
     wave_buffering_thread.start()
+
     while stream.is_active() and video.isOpened():
-        if len(buffer) > 0:
-            wave, frame, prepared = buffer.popleft()
-            stream.write(wave_to_data(wave))
-
-            if SHOW_VIDEO:
-                resized_prepared = cv2.cvtColor(
-                    cv2.resize(prepared, frame.shape[1::-1]),  # y, x = frame.shape[1::-1]
-                    cv2.COLOR_GRAY2RGB
-                )
-                concat = cv2.vconcat([frame, resized_prepared])
-
-                if SHOW_PLOT:
-                    if FOURIER_TRANSFORM_WAVE:
-                        wave = np.fft.rfft(wave).real
-
-                    plot = cv2.cvtColor(cv2.resize(plot_to_ndarray(wave), concat.shape[1::-1]),
-                                        cv2.COLOR_RGBA2RGB)
-                    cv2.imshow("Video", cv2.hconcat([concat, plot]))
-                else:
-                    cv2.imshow("Video", concat)
-
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    break
-
-            elif SHOW_PLOT:
-                if FOURIER_TRANSFORM_WAVE:
-                    wave = np.fft.rfft(wave).real
-                plt.clf()
-                plt.plot(wave)
-                plt.pause(0.0001)
-
-        if len(buffer) > MAX_BUFFER:
-            buffer = buffer[int(MAX_BUFFER / 2):]
+        if not stream_from_buffer(stream, buffer):
+            break
 
     cv2.destroyAllWindows()
     video.release()
     stream.stop_stream()
     stream.close()
-    pa.terminate()
+    audio.terminate()
+    wave_buffering_thread.join()
 
 
 def main(argv):
+    """
+    Main function. Reads sys.argv and runs synesthesia function based on the input
+    :param argv:
+    """
     def usage():
         print("Usage: " + sys.argv[0] + " -i <input path>")
-    input_path = None
 
+    input_path = None
     try:
-        opts, args = getopt.getopt(argv[1:], "hi:", ["help", "inpath="])
-    except getopt.GetoptError as e:
+        opts, _ = getopt.getopt(argv[1:], "hi:", ["help", "inpath="])
+    except getopt.GetoptError as error:
         usage()
-        sys.exit(e)
+        sys.exit(error)
     for opt, arg in opts:
         if opt == '-h':
             usage()
@@ -243,7 +295,7 @@ def main(argv):
             input_path = arg
     if input_path is None:
         usage()
-        exit(1)
+        sys.exit(1)
     synesthesia(input_path)
 
 
